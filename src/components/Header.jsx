d8f9635b776e0aaccb03cd87d6365458e2ccb247
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-06-10 13:30:44 +0100

    remove the hard code JSX icon and import it from Icon component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import darkLogo from "../assets/darkLogo.png";
+import { roundedUserIcon, menuIcon, closeIcon } from "../components/Icons";
 
 function Header() {
   // Styling for the navigation links
@@ -39,21 +40,7 @@ function Header() {
           className={`m-2 justify-center border-tc flex items-center border rounded-lg p-1`}
         >
           {/* User icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
-
+          {roundedUserIcon()}
           <span className="pl-1">login</span>
         </Link>
       </div>
@@ -65,44 +52,6 @@ function Header() {
     setDisplyMenu((prevDisplyMenu) => !prevDisplyMenu);
   };
 
-  const closeIcon = () => {
-    // cancel icon
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={2}
-        stroke="currentColor"
-        className="w-8 h-8"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 18L18 6M6 6l12 12"
-        />
-      </svg>
-    );
-  };
-  const menuIcon = () => {
-    // Menu icon
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={2}
-        stroke="currentColor"
-        className="w-8 h-8"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-        />
-      </svg>
-    );
-  };
   return (
     <header className="flex sticky top-0 justify-center border-tc border-b text-tc bg-slate-300">
       <div>
@@ -118,7 +67,7 @@ function Header() {
 
           {/* Mobile menu button */}
           <button onClick={showMenu} className="block md:hidden">
-            {displyMenu ? closeIcon() : menuIcon()}
+            {displyMenu ? closeIcon(2, 8, 8) : menuIcon(2, 8, 8)}
           </button>
         </div>
 
