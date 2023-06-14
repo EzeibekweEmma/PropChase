2a0f03993065f42b71d75768d7ad4f4a221ed517
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-06-14 12:15:32 +0100

    create a user model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create the User model
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
