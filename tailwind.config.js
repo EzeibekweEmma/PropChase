69147442547a5856e2b3ae20535482d8f472ca55
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-06-17 20:36:37 +0100

    add light backgroung color and light text color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     minHeight: {
-      startingH :'80vh',
+      startingH: "80vh",
     },
     extend: {
       colors: {
         bgc: "#ffffff",
+        lbgc: "#cbd5e1",
         tc: "#001a33",
+        ltc: "#64748b",
       },
     },
   },
