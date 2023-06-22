9950cb104f1e6682bf5977358703ab9ae94eb893
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-06-22 06:26:55 +0100

    Added place Model for add new property

---
+++ models/Place.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.PlaceSchema({
+    owner: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+    title: String,
+    address: String,
+    photos: [String],
+    description: String,
+    perks: [String],
+    extraInfo: String,
+    checkIn: Number,
+    checkOut: Number,
+    maxGuests: Number,
+});
+
+const PlaceModel = new mongoose.model('Place', placeSchema);
+
+module.exports = PlaceModel;
