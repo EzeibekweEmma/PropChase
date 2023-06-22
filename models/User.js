79cc97270291d1930af1bedf126e66b10835fd80
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-06-22 06:28:52 +0100

    added two fields avater and description

--- models/User.js
+++ models/User.js
@@ -15,6 +15,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  avater: String,
+  description: String,
   createdAt: {
     type: Date,
     default: Date.now,
