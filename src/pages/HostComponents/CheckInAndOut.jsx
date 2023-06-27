bf4a21a5ee00a6a67a91d00a0775a2ef76cfdcd7
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-06-27 23:52:14 +0100

    added price section to CheckInAndCheckOut component

--- src/pages/HostComponents/CheckInAndOut.jsx
+++ src/pages/HostComponents/CheckInAndOut.jsx
@@ -1,4 +1,4 @@
-import { ClockIcon, UserGroupIcon } from "@heroicons/react/24/outline";
+import { ClockIcon, CurrencyDollarIcon, UserGroupIcon } from "@heroicons/react/24/outline";
 
 export default function checkInAndOut({ headerText, handleChange, formData }) {
   // Check In and Check Out Time
@@ -8,7 +8,7 @@ export default function checkInAndOut({ headerText, handleChange, formData }) {
         "Check In and Check Out Time",
         "Add your check-in and check-out time, remember to have some time window for cleaning the room between guests."
       )}
-      <div className="grid gap-2 sm:grid-cols-3 mt-2">
+      <div className="grid gap-2 grid-cols-2 md:sm:grid-cols-4 mt-2">
         {/* Check-in Time */}
         <label className="text-sm relative block">
           <p className="">Check-in time</p>
@@ -53,13 +53,31 @@ export default function checkInAndOut({ headerText, handleChange, formData }) {
           </span>
           <input
             className="min-w-full rounded-xl bg-slate-100 py-1 pr-3 mb-4
-                focus:outline-none placeholder:italic focus:shadow-md indent-8"
+                focus:outline-none placeholder:italic focus:shadow-md indent-14"
             type="number"
             onChange={handleChange}
             name="maxGuests"
             value={formData.maxGuests}
             required
             placeholder="1"
+          />
+        </label>
+
+        {/* Price pre night */}
+        <label className="text-sm relative block">
+          <p className="">Price pre night</p>
+          <span className="absolute inset-y-6 left-0 pl-2">
+            <CurrencyDollarIcon className="h-5 w-5" />
+          </span>
+          <input
+            className="min-w-full rounded-xl bg-slate-100 py-1 pr-3
+                focus:outline-none placeholder:italic focus:shadow-md indent-14"
+            type="number"
+            onChange={handleChange}
+            name="price"
+            value={formData.price}
+            required
+            placeholder="10"
           />
         </label>
       </div>
