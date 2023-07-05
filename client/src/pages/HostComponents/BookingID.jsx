bd63944fb25f620aef31e1bcaa7e3a20b8e291e8
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-07-05 13:46:48 +0100

    Add BookingID component to display booking details The code defines a React functional component named BookingID that displays booking details based on the provided id. It utilizes various dependencies like axios, useContext, useEffect, and useState. The component also uses icons from the Heroicons library and the date-fns library for date manipulation.
    The component fetches booking details from the server using the axios library and displays various information, such as the property title, address, number of nights, number of guests, check-in and check-out dates, and total price. Additionally, it renders a group of photos related to the property and allows the user to view all photos when clicked.
    
    The code includes handling for when the user is not logged in, and it redirects to the login page using the Navigate component from React Router. The component also uses conditional rendering based on the showAllPhotos state to display all photos in a separate AllPhotos component.
    
    Overall, the BookingID component enhances the user experience by providing detailed information about a specific booking.

--- client/src/pages/HostComponents/BookingID.jsx
+++ client/src/pages/HostComponents/BookingID.jsx
@@ -2,11 +2,16 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { UserContext } from "../../components/UserContext";
+import { ArrowRightIcon, CalendarDaysIcon, MapPinIcon, MoonIcon, UserGroupIcon } from "@heroicons/react/24/outline";
+import { differenceInCalendarDays, format } from "date-fns";
+import PhotoCard from "../../components/PhotoCard";
+import AllPhotos from "../../components/AllPhotos";
 
 export default function BookingID() {
   const { id } = useParams();
   const [details, setDetails] = useState(null);
   const { user } = useContext(UserContext);
+  const [showAllPhotos, setShowAllPhotos] = useState(false);
 
   useEffect(() => {
     if (user) {
@@ -18,6 +23,14 @@ export default function BookingID() {
       });
     }
   }, [id, user]);
+    if (showAllPhotos) {
+      return (
+        <AllPhotos
+          data={details.property}
+          setShowAllPhotos={setShowAllPhotos}
+        />
+      );
+    }
 
   // if the user is not logged in navigate to the login page
   if (!user) return <Navigate to="/login" />;
@@ -26,7 +39,79 @@ export default function BookingID() {
     <main>
       <section className="flex justify-center">
         <section className=" w-[80vw] min-h-[48vh] my-5">
-          {details && <div>BookingID {id}</div>}
+          {details ? (
+            <section>
+              <div>
+                <h1 className="text-2xl font-semibold">
+                  {details.property.title}
+                </h1>
+                <a
+                  className="flex items-center space-x-1 w-fit text-xs font-medium underline"
+                  target="_blank"
+                  href={`https://maps.google.com/?q=${details.property.address}`}
+                  rel="noreferrer"
+                >
+                  <MapPinIcon className="h-4 w-4" />
+                  <span>{details.property.address}</span>
+                </a>
+              </div>
+              <div
+                className="flex flex-col sm:flex-row justify-between sm:items-center
+              bg-lbgc p-4 rounded-xl my-4 space-y-3 sm:space-y-0"
+              >
+                <div>
+                  <h3 className="text-md sm:text-xl font-medium sm:font-semibold">
+                    Your booking information:
+                  </h3>
+                  <div>
+                    {/* Number of nights and guests */}
+                    <div className="flex items-center text-xs my-1 text-ltc sm:text-sm space-x-2">
+                      <p className="flex items-center space-x-1">
+                        <MoonIcon className="h-4 w-4" />
+                        <span>
+                          {differenceInCalendarDays(
+                            new Date(details.checkOut),
+                            new Date(details.checkIn)
+                          )}{" "}
+                          Night
+                        </span>
+                      </p>
+                      <p className="flex items-center space-x-1">
+                        <UserGroupIcon className="h-4 w-4" />
+                        <span>{details.numberOfGuests} Guest</span>
+                      </p>
+                    </div>
+                    {/* Check-in and Check-out dates */}
+                    <div className="flex items-center text-xs my-1 text-ltc sm:text-sm space-x-2">
+                      <p className="flex items-center space-x-1">
+                        <CalendarDaysIcon className="h-4 w-4" />
+                        <span>
+                          {format(new Date(details.checkIn), "yyyy-MM-dd")}
+                        </span>
+                      </p>
+                      <ArrowRightIcon className="h-3 w-3 stroke-2" />
+                      <p className="flex items-center space-x-1">
+                        <CalendarDaysIcon className="h-4 w-4 " />
+                        <span>
+                          {format(new Date(details.checkOut), "yyyy-MM-dd")}
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="text-center p-3 bg-tc text-bgc rounded-xl">
+                  <h3 className="text-sm sm:text-base">Total price</h3>
+                  <h1 className="text-lg sm:text-2xl">${details.price}</h1>
+                </div>
+              </div>
+              {/* Group of photos preview*/}
+              <PhotoCard
+                data={details.property}
+                showAllPhotos={showAllPhotos}
+                setShowAllPhotos={setShowAllPhotos}
+              />
+            </section>
+          ) : null}
         </section>
       </section>
     </main>
